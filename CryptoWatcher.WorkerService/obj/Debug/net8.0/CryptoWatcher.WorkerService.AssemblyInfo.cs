01f299a5e538261cb77b1fb5abc7a5346247c9e7
cootoonhoo <cootoonhoo@gmail.com>
2024-12-16 09:41:39 -0300

    Creating frontend app

--- CryptoWatcher.WorkerService/obj/Debug/net8.0/CryptoWatcher.WorkerService.AssemblyInfo.cs
+++ CryptoWatcher.WorkerService/obj/Debug/net8.0/CryptoWatcher.WorkerService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoWatcher.WorkerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e40763fc241c8d88ec23c9345c4f6a8a22ceb8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f80928ff2289e0a95384212f58463843415d9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoWatcher.WorkerService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoWatcher.WorkerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
